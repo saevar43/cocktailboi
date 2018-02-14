12febcd0fcaa205ea82954def252c3646919e4f9
Sævar Ingi Sigurðsson <saevar43@gmail.com>
2018-02-14 12:11:35 +0000

    First commit

---
+++ app/src/main/java/hi/hbv601g/cocktailboi/MainActivity.java
@@ -0,0 +1,13 @@
+package hi.hbv601g.cocktailboi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
