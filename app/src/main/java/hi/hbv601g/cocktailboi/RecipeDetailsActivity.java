a77cba803cf0eb31af951430de1ee60348054990
unknown <karigegu@gmail.com>
2018-03-10 16:03:04 +0000

    Added look for MainActivity

---
+++ app/src/main/java/hi/hbv601g/cocktailboi/RecipeDetailsActivity.java
@@ -0,0 +1,13 @@
+package hi.hbv601g.cocktailboi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
